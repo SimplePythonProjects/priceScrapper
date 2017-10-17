c563aaa034e10a57c6aecd49a5b688e2d4e3be6b
Ubuntu <ubuntu@ip-172-31-31-93.ec2.internal>
2017-10-17 07:40:31 +0000

    Changes made to support the aws OS environment. Follow the steps in the below link to setup the chrome webdriver in ubuntu https://christopher.su/2015/selenium-chromedriver-ubuntu/

--- index.py
+++ index.py
@@ -1,12 +1,19 @@
+from pyvirtualdisplay import Display
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
+import telebot
+
+bot = telebot.TeleBot("410814681:AAEAsNsqywiZ4C8iFrwZbl-DdyEHUPmlcww")
 
 coins = ['ethereum','bitcoin', 'litecoin', 'ripple', 'tenx', 'stratis', 'golem-network-tokens', 'bitquence']
 def getDetails():
     #Logic to scrape the site and get the prices.
+    display = Display(visible=0, size=(800, 600))
+    display.start()
     base_url = "https://coinmarketcap.com/currencies/"
-    browser = webdriver.Chrome('/Users/rgeorgewinsathia/PycharmProjects/priceScrapper/ChromeDriverFolder/chromedriver')
+    browser = webdriver.Chrome()
+    result = ""
 
     for coin in coins:
         url = base_url + coin
@@ -23,9 +30,16 @@ def getDetails():
         btc_price = usd_change.find_next_sibling('small')
         btc_change = btc_price.find_next_sibling('small')
 
-        print coin
-        print usd_price.text + "\t" + usd_change.text + "\n"
-        print btc_price.text + "\t" + btc_change.text + "\n"
+        result += coin + "\n"
+        result += usd_price.text + "\t" + usd_change.text + "\n"
+        result += btc_price.text + "\t" + btc_change.text + "\n \n"
+    return result
 
-getDetails()
+def sendMessage(data):
+    #Write code to send the message
+    id = -1001123624076
+    bot.send_message(id, data)
 
+result = getDetails()
+#print result
+sendMessage(result)
